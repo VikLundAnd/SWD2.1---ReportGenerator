f20f7a0e5d81cb260acfb25bce7a479eb793dffc
Viktor Lundsgaard Andersen <vik.lund.and@gmail.com>
2020-02-04 12:05:38 +0100

    Add project files.

---
+++ SOLID 1 - ReportGenerator/ReportGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReportGenerator
+{
+    class ReportGenerator
+    {
+        protected List<Employee> generateList(EmployeeDB _employeeDB)
+        {
+            var employees = new List<Employee>();
+            Employee employee;
+
+            _employeeDB.Reset();
+
+            // Get all employees
+            while ((employee = _employeeDB.GetNextEmployee()) != null)
+            {
+                employees.Add(employee);
+            }
+            return employees;
+        }
+    }
+
+    class ReportGenerator_salaryfirst : ReportGenerator
+    {
+        
+        public  void CompileReport(EmployeeDB _employeeDB)
+        {
+            var employees = base.generateList(_employeeDB);
+
+            Console.WriteLine("Salary-first report");
+            foreach (var e in employees)
+            {
+                Console.WriteLine("------------------");
+                Console.WriteLine("Salary: {0}", e.Salary);
+                Console.WriteLine("Name: {0}", e.Name);
+                Console.WriteLine("------------------");
+            }
+        }
+    }
+
+    class ReportGenerator_namefirst : ReportGenerator
+    {
+        public void CompileReport(EmployeeDB _employeeDB)
+        {
+            var employees = base.generateList(_employeeDB);
+
+            Console.WriteLine("Name-first report");
+            foreach (var e in employees)
+            {
+                Console.WriteLine("------------------");
+                Console.WriteLine("Name: {0}", e.Name);
+                Console.WriteLine("Salary: {0}", e.Salary);
+                Console.WriteLine("------------------");
+            }
+        }
+    }
+}
---
+++ SOLID 1 - ReportGenerator/ReportGeneratorClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ReportGenerator
+{
+    internal class ReportGeneratorClient
+    {
+        private static void Main()
+        {
+            var db = new EmployeeDB();
+
+            // Add some employees
+            db.AddEmployee(new Employee("Anne", 3000));
+            db.AddEmployee(new Employee("Berit", 2000));
+            db.AddEmployee(new Employee("Christel", 1000));
+
+
+            // Create a default (name-first) report
+            var reportGenerator = new ReportGenerator_namefirst();
+            reportGenerator.CompileReport(db);
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+                
+            // Create a salary-first report
+            var reportGenerator2 = new ReportGenerator_salaryfirst();
+            reportGenerator2.CompileReport(db);
+        }
+    }
+}
